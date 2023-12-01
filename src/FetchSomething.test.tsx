454446f7a7d3fc89f825b3b6428b35eb48e7f880
Nicolai Süper <n.sueper@micromata.de>
2023-12-01 11:31:32 +0100

    test husky/lint-staged

--- src/FetchSomething.test.tsx
+++ src/FetchSomething.test.tsx
@@ -1,43 +1,42 @@
-import {describe, expect, it} from "vitest";
-import {render, waitFor, screen} from "@testing-library/react";
-import {FetchSomething} from "./FetchSomething.tsx";
-import {SouthParkAPIResponse} from "./types.ts";
-import nock from "nock";
+import { describe, expect, it } from "vitest";
+import { render, waitFor, screen } from "@testing-library/react";
+import { FetchSomething } from "./FetchSomething.tsx";
+import { SouthParkAPIResponse } from "./types.ts";
 
 const mockReply: SouthParkAPIResponse = {
-    data: [{
-        "id": 1,
-        "name": "Hurzel",
-        "age": null,
-        "sex": "Male",
-        "hair_color": "Brown",
-        "occupation": "Lawyer",
-        "grade": null,
-        "religion": "Judaism",
-        "voiced_by": null,
-        "created_at": "2022-03-10T17:02:56.000000Z",
-        "updated_at": "2022-03-10T17:02:56.000000Z",
-        "url": "https://spapi.dev/api/characters/1",
-        "family": "https://spapi.dev/api/families/1",
-    }],
-    links: {
-        first: "",
-        last: "",
-        next: "",
-        prev: ""
+  data: [
+    {
+      id: 1,
+      name: "Gerald Broflovski",
+      age: null,
+      sex: "Male",
+      hair_color: "Brown",
+      occupation: "Lawyer",
+      grade: null,
+      religion: "Judaism",
+      voiced_by: null,
+      created_at: "2022-03-10T17:02:56.000000Z",
+      updated_at: "2022-03-10T17:02:56.000000Z",
+      url: "https://spapi.dev/api/characters/1",
+      family: "https://spapi.dev/api/families/1",
     },
-    meta: {}
+  ],
+  links: {
+    first: "",
+    last: "",
+    next: "",
+    prev: "",
+  },
+  meta: {},
 } as never as SouthParkAPIResponse;
 
-describe('<FetchSomething />', () => {
-    it('should render without crashing', async () => {
-        nock("https://spapi.dev")
-            .get("/api/characters")
-            .reply(200, mockReply);
+describe("<FetchSomething />", () => {
+  it("should render without crashing", async () => {
+    fetchMock.mockResponseOnce(JSON.stringify(mockReply));
 
-        const {findByText} = render(<FetchSomething />);
-        const element  = await waitFor(() => findByText(/Gerald/));
-        screen.debug(element);
-        expect(element).toBeInTheDocument();
-    });
+    const { findByText } = render(<FetchSomething />);
+    const element = await waitFor(() => findByText(/Gerald/));
+    screen.debug(element);
+    expect(element).toBeInTheDocument();
+  });
 });
--- test-setup.ts
+++ test-setup.ts
@@ -1,6 +1,13 @@
-import { afterEach } from 'vitest';
+import { afterEach, vi } from 'vitest';
+import createFetchMock from 'vitest-fetch-mock';
 import { cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/vitest';
+
+
+const fetchMocker = createFetchMock(vi);
+
+// sets globalThis.fetch and globalThis.fetchMock to our mocked version
+fetchMocker.enableMocks();
 
 // runs a cleanup after each test case (e.g. clearing jsdom)
 afterEach(() => {
